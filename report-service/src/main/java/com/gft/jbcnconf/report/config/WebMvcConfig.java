12ccf7923ce502d8306ac630af603fbbd99c73b9
Marcelo Colomer <mocr@gft.com>
2017-06-21 00:58:16 +0200

    report microservice template

---
+++ report-service/src/main/java/com/gft/jbcnconf/report/config/WebMvcConfig.java
@@ -0,0 +1,50 @@
+package com.gft.jbcnconf.report.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * Web Configuration
+ * @author MOCR
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    private ObjectMapper objectMapper;
+
+    public WebMvcConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+    /**
+     * Message converters configuration.
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);        
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
+    }
+ 
+    /**
+     * Content negotiation configuration.
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
---
+++ report-service/src/test/java/com/gft/jbcnconf/report/test/config/MongoConfiguration.java
@@ -0,0 +1,29 @@
+package com.gft.jbcnconf.report.test.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.Mongo;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.gft.jbcnconf.report.repository")
+public class MongoConfiguration extends AbstractMongoConfiguration { 
+
+    @Override
+    protected String getDatabaseName() {
+        return "test";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new Fongo(getDatabaseName()).getMongo();
+    }
+    
+    @Override
+    protected String getMappingBasePackage() {
+        return "com.gft.jbcnconf.report.domain";
+    } 
+    
+}
